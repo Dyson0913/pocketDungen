87a096c3a1e7965049a55cf9512fdb283606ca86
Dyson0913 <hhg4092@gmail.com>
2017-02-08 01:48:02 +0800

    button

--- PocketDungen/src/TestUI.js
+++ PocketDungen/src/TestUI.js
@@ -1,14 +1,26 @@
+
+var Event = Laya.Event;
+
 function TestUI()
 {
 	TestUI.super(this);
 	
+	this.btn_ok.on(Event.CLICK, this, onBtnClick);
+	this.btn_cancel.on(Event.CLICK, this, onBtnClick2);
+
 	function onBtnClick()
 	{
-		//手动控制组件属性
-		
+		trace("1111");
+		this.showText.text = "111";
 	}
 
-	
+	function onBtnClick2()
+	{
+		trace("2222");
+		this.showText.text = "222";
+	}
+
+
 }
 
 Laya.class(TestUI, "TestUI", TestPageUI);
--- PocketDungen/src/ui/layaUI.max.all.js
+++ PocketDungen/src/ui/layaUI.max.all.js
@@ -5,6 +5,9 @@ var Dialog=laya.ui.Dialog;
 var TestPageUI=(function(_super){
 		function TestPageUI(){
 			
+		    this.showText=null;
+		    this.btn_ok=null;
+		    this.btn_cancel=null;
 
 			TestPageUI.__super.call(this);
 		}
@@ -18,7 +21,7 @@ var TestPageUI=(function(_super){
 		}
 
 		STATICATTR$(TestPageUI,
-		['uiView',function(){return this.uiView={"type":"View","props":{"width":600,"height":400},"child":[{"type":"Label","props":{"y":124,"x":162,"width":137,"text":"loading","height":87,"color":"#f1e1e1"}},{"type":"Image","props":{"y":243,"x":142,"skin":"res/loading/chip_1.png"}}]};}
+		['uiView',function(){return this.uiView={"type":"View","props":{"width":600,"height":400},"child":[{"type":"Label","props":{"y":124,"x":162,"width":137,"var":"showText","text":"loading","height":87,"color":"#f1e1e1"}},{"type":"Image","props":{"y":243,"x":142,"skin":"res/loading/chip_1.png"}},{"type":"Button","props":{"y":35,"x":137,"var":"btn_ok","skin":"res/loading/Btn_bg.png","labelStrokeColor":"#f6f6f6","label":"test"}},{"type":"Button","props":{"y":32,"x":309,"var":"btn_cancel","skin":"res/loading/Btn_bg.png","labelStrokeColor":"#f6f6f6","label":"cancel"}}]};}
 		]);
 		return TestPageUI;
 	})(View);
